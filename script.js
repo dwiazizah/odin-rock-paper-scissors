b5a0462f86aa5c52cb691da1fa474774a9f2da64
Azizah <azizahdwi13@gmail.com>
2022-09-28 23:14:04 +0700

    Add playRound function

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
 
-    return computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice = choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Paper" || 
+        playerSelection == "Paper" && computerSelection == "Scissors" || 
+        playerSelection == "Scissors" && computerSelection == "Rock") {
+            return result = "You lose! " + computerSelection + " beats " + playerSelection + ".";
+    }  else if (playerSelection == "Rock" && computerSelection == "Scissors" ||
+                playerSelection == "Paper" && computerSelection == "Rock" ||
+                playerSelection == "Scissors" && computerSelection == "Paper") {
+                    return result = "You win! " + playerSelection + " beats " + computerSelection + ".";
+    } else {
+        return result = "You tied! Play again."
+    }
 }
